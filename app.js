4a462969859154bd41168f297c88f89bdcbd29df
bumbitzu <bumbitzu@users.noreply.github.com>
2025-02-23 22:38:26 +0200

    fresh update

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const express = require('express');
-const { execFile } = require('child_process');
 const path = require('path');
 const app = express();
-const port = 3001;
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static(path.join(__dirname, '../public_html')));
 app.use(express.json());
 
-app.post('/calculator/calculate', (req, res) => {
-    const { formula } = req.body;
+// Setăm EJS ca motor de vizualizare
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-    const calculatorPath = path.join(__dirname, 'calculator');
-    execFile(calculatorPath, [formula], (error, stdout, stderr) => {
-        if (error) {
-            // log to error to file
-            console.error(error);
-            return res.status(500).json({ error: stderr || error.message });
-        }
-        // log to file
-        console.log(`Result: ${stdout.trim()}`);
-        res.json({ result: stdout.trim() });
-    });
+app.get('/calculator', (req, res) => {
+    res.render('index');
 });
 
 app.listen(port, () => {
